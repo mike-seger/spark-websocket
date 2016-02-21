c47aa0d0070a2d59b2b0b307dcd5063b33a448c9
mike-seger <michael@ivy-i7-2.lan>
2016-02-21 04:03:31 +0100

    some minor fixes

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,5 +1,7 @@
 import org.eclipse.jetty.websocket.api.*;
 import org.json.*;
+
+import java.io.File;
 import java.text.*;
 import java.util.*;
 import static j2html.TagCreator.*;
@@ -11,15 +13,18 @@ public class Chat {
     static int nextUserNumber = 1; //Assign to username for next connecting user
 
     public static void main(String[] args) {
+        port(determinePort());
         staticFileLocation("public"); //index.html is served at localhost:4567 (default port)
         webSocket("/chat", ChatWebSocketHandler.class);
+        webSocketIdleTimeoutMillis(600*1000);
+        //secure(keystoreFile, keystorePassword, truststoreFile, truststorePassword);
         init();
     }
 
     //Sends a message from one user to all users, along with a list of current usernames
     public static void broadcastMessage(String sender, String message) {
         userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
-            try {
+            try {	
                 session.getRemote().sendString(String.valueOf(new JSONObject()
                     .put("userMessage", createHtmlMessageFromSender(sender, message))
                     .put("userlist", userUsernameMap.values())
@@ -37,6 +42,17 @@ public class Chat {
                 p(message),
                 span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
         ).render();
+    }
+
+    static int determinePort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) { //Heroku
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        } else if(new File("manifest.yml").exists() || new File("/var/www").exists() ) {
+        		//ipAddress("127.0.0.1");
+        		return 18080;
+        }
+        return 4443; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
